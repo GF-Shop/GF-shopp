de7d78a467ad7c2e07b6a332eb70aff6d68b1d23
Fatma Miled <you@example.com>
2022-12-07 14:55:41 +0100

    done

--- gf-shop/src/component/Help.jsx
+++ gf-shop/src/component/Help.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import "./Help.css";
 
 const Help = () => {
   return (
-    <div>Help</div>
-  )
-}
+    <div>
+      <div className="about-section">
+        <h1>About Us </h1>
+        <h5>
+          G-F-S :
+          Green Field Shop is a platform that allows the users to buy and
+          sell all the categories of products this is a project whish is
+          Customer to customer (C2C) is a business model whereby customers can
+          trade with each other, typically in an online environment. Two
+          implementations of C2C markets are auctions and classified
+          advertisements. C2C marketing has soared in popularity with the
+          arrival of the internet prices .
+        </h5>
+        <p>
+          Your Confort is Our Priority
+        </p>
+      </div>
 
-export default Help
+      <h2>Our Team</h2>
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <img
+              src="/w3images/team1.jpg"
+              alt=""
+              style={{ width: "100%" }}
+            />
+            <div className="container">
+              <h2>Walid Slim</h2>
+              <p className="title">CEO & Founder</p>
+              
+              <p>walid-sliim@gmail.com</p>
+              
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img src="" alt="" style={{ width: "100%" }} />
+            <div className="container">
+              <h2>Aziz Selini</h2>
+              <p className="title">Art Director</p>
+         
+              <p>aziz.selini@gmail.com</p>
+            
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img
+              src="/w3images/team3.jpg"
+              alt=""
+              style={{ width: "100%" }}
+            />
+            <div className="container">
+              <h2>Abderrahim Ouertani</h2>
+              <p className="title">Scrum Master</p>
+             
+              <p>Abderrahim.Ouertani@gmail.com</p>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Help;
